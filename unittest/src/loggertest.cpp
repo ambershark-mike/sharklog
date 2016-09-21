131b8dabee130045ec32ececd03bc1e6393cceb9
Mike Ryan <public@ambershark.com>
2016-09-20 17:50:57 -0700

    LOG-1: Adds copyleft to loggertest

--- unittest/src/loggertest.cpp
+++ unittest/src/loggertest.cpp
@@ -1,3 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2016, by Ambershark, LLC.
+//
+// Distributed under the L-GPL license.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You sould have received a copy of the GNU Lesser General Public
+// License along with this program.  If not see
+// <http://www.gnu.org/licenses>.
+//
+// This notice must remain in the source code and any derived source.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include "loggertest.h"
 #include "logger.h"
 #include <iostream>
@@ -212,4 +236,11 @@ TEST_F(LoggerTest, ClosingAndMakingANewLoggerWorks)
     EXPECT_TRUE((bool)n);
     
     ASSERT_NE(old, n);
+}
+
+TEST_F(LoggerTest, DISABLED_LoggerNamesAreCaseInsensitive)
+{
+    auto logger = Logger::logger("Mike");
+    EXPECT_TRUE((bool)logger);
+    ASSERT_EQ(logger, Logger::logger("mike"));
 }
