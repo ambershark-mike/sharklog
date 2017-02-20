0196f60800f0cd96204173a912fb318350c87294
Mike Ryan <public@ambershark.com>
2017-02-19 17:33:59 -0700

    LOG43: adds vagrant for win10 build

---
+++ provision/chocolatey.ps1
@@ -0,0 +1,6 @@
+# install chocolatey
+$ChocoInstallPath = "$env:SystemDrive\ProgramData\Chocolatey\bin"
+
+if (!(Test-Path $ChocoInstallPath)) {
+	iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+}
---
+++ provision/packages.bat
@@ -0,0 +1,7 @@
+chocolatey feature enable -n=allowGlobalConfirmation
+choco install vcbuildtools
+choco install cygwin
+choco install cyg-get
+choco install mingw
+choco install cmake
+chocolatey feature disable -n=allowGlobalConfirmation
